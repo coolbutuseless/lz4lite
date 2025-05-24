24b25d1f66ac753c004383981f9f36938af5ee4c
mike <mikefc@coolbutuseless.com>
2025-05-24 10:47:07 +1000

    prep for lz4 streaming

--- src/lz4-serialize-stream.c
+++ src/lz4-serialize-stream.c
@@ -25,7 +25,9 @@ typedef struct {
   uint32_t data_length;
   LZ4_stream_t       *stream_out;
   LZ4_streamDecode_t *stream_in;
+  
   uint8_t *comp;
+  int comp_capacity;
 } dbuf_t;
 
 
@@ -41,8 +43,23 @@ typedef struct {
 void db_destroy(dbuf_t *db) {
   if (db->file != NULL) {
     if (db->fmode == FMODE_WRITE) {
+#ifdef USE_LZ4
+      int comp_len = LZ4_compress_fast_continue(
+        db->stream_in,     // Stream
+        db->buf[db->idx],  // Source Raw Buffer
+               db->comp,          // Dest Compressed buffer
+               db->pos,           // Source size
+               db->comp_capacity, // dstCapacity
+               1                  // acceleration
+      );
+      if (comp_len < 0) Rf_error("Error compression lz4");
+      fwrite(&db->pos, 1, sizeof(uint32_t), db->file); // Write raw length
+      fwrite(&comp_len, 1, sizeof(int32_t), db->file); // write compressed length
+      fwrite(db->comp, 1, comp_len, db->file);  // Write compressed buffer
+#else
       fwrite(&db->pos, 1, sizeof(uint32_t), db->file);
       fwrite(db->buf[db->idx], 1, db->pos, db->file);
+#endif
     }
     fclose(db->file);
   } else {
@@ -84,8 +101,26 @@ void write_bytes_stream(R_outpstream_t stream, void *src, int length) {
   // Remember: We need to keep some historical bytes around so that
   // the lz4 compression has a data reference
   if (db->pos + length > BUF_SIZE) {
+    
+#ifdef USE_LZ4
+    
+    int comp_len = LZ4_compress_fast_continue(
+      db->stream_in,     // Stream
+      db->buf[db->idx],  // Source Raw Buffer
+      db->comp,          // Dest Compressed buffer
+      db->pos,           // Source size
+      db->comp_capacity, // dstCapacity
+      1                  // acceleration
+    );
+    if (comp_len < 0) Rf_error("Error compression lz4");
+    fwrite(&db->pos, 1, sizeof(uint32_t), db->file); // Write raw length
+    fwrite(&comp_len, 1, sizeof(int32_t), db->file); // write compressed length
+    fwrite(db->comp, 1, comp_len, db->file);  // Write compressed buffer
+    
+#else
     fwrite(&db->pos, 1, sizeof(uint32_t), db->file); // Write length
     fwrite(db->buf[db->idx], 1, db->pos, db->file);  // Write buffer
+#endif
     db->idx = 1 - db->idx; // switch buffers
     db->pos = 0;           // reset buffer position
   } 
@@ -134,7 +169,8 @@ SEXP lz4_serialize_stream_(SEXP x_, SEXP dst_) {
   
   // Setup LZ4 encoding stream context
   db->stream_out = LZ4_createStream();
-  db->comp = malloc(LZ4_COMPRESSBOUND(BUF_SIZE));
+  db->comp_capacity = LZ4_COMPRESSBOUND(BUF_SIZE);
+  db->comp = malloc(db->comp_capacity);
   if (db->comp == NULL) {
     Rf_error("lz4_serialize_stream() couldnt allocate compressed buffer");
   }
@@ -195,11 +231,26 @@ void read_bytes_stream(R_inpstream_t stream, void *dst, int length) {
     db->pos = 0;           // Reset position
     
     // Read buffer length, then read buffer data
+#ifdef USE_LZ4
+    int comp_len;
+    fread(&db->data_length, 1, sizeof(uint32_t), db->file);
+    fread(&comp_len, 1, sizeof(int32_t), db->file);
+    fread(db->comp, 1, comp_len, db->file);
+    int res = LZ4_decompress_safe_continue(
+      db->stream_in, 
+      db->comp, 
+      db->buf[db->idx],
+      comp_len,
+      BUF_SIZE
+    );
+    
+#else
     fread(&db->data_length, 1, sizeof(uint32_t), db->file);
     unsigned long nread = fread(db->buf[db->idx], 1, db->data_length, db->file);
     if (nread != db->data_length) {
       Rf_error("Read failed: %i/%i", (int)nread, db->data_length);
     }
+#endif
   }
   
   // copy across bytes
@@ -238,7 +289,8 @@ SEXP lz4_unserialize_stream_(SEXP src_) {
   
   // LZ4 stream handling
   db->stream_in = LZ4_createStreamDecode();
-  db->comp = malloc(LZ4_COMPRESSBOUND(BUF_SIZE));
+  db->comp_capacity = LZ4_COMPRESSBOUND(BUF_SIZE);
+  db->comp = malloc(db->comp_capacity);
   if (db->comp == NULL) {
     Rf_error("lz4_unserialize_stream() couldnt allocate compressed buffer");
   }
